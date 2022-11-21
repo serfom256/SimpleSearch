dcf5112f011167a1f80978f23fddc246edd30407
Misha_Tsarytsanski <miha.18012006@gmail.com>
2022-11-21 02:05:44 +0100

    Implemented docx reader with bug

--- src/main/java/com/opensearch/util/reader/DocxReader.java
+++ src/main/java/com/opensearch/util/reader/DocxReader.java
@@ -13,19 +13,26 @@ import java.util.Map;
 public class DocxReader extends AbstractReader {
     @Override
     public Map<String, List<Document>> read(File file, List<Character> regex) {
+        return parseText(readDocx(file), file, regex);
+    }
+
+    private String readDocx(File file) {
         try {
             FileInputStream fis = new FileInputStream(file.getAbsolutePath());
 
             XWPFDocument document = new XWPFDocument(fis);
 
+            String resultStr = "";
+
             List<XWPFParagraph> paragraphs = document.getParagraphs();
             for (XWPFParagraph para : paragraphs) {
-                System.out.println(para.getText());
+                resultStr = resultStr + " " + para.getText();
             }
             fis.close();
+            return resultStr;
         } catch (Exception e) {
             e.printStackTrace();
+            return "";
         }
-        return Collections.emptyMap();
     }
 }
